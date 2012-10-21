c1c0a00de3253ec6ef27128c519d562ec71deb19
Zachary Kessin <zkessin@gmail.com>
2012-10-21 09:27:11 +0200

    update html

---
+++ Backbone/list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+bs 01a_testing_selenium
+bs 01b_testing_qunit
+bs 01c_testing_jasmine
+bs 01d_testing_phantomjs
+bs 01e_testing_tdd
+bs 02_underscore
+bs 03_backbone
+bs 04_backbone_marionette
+ba adjenda
+
